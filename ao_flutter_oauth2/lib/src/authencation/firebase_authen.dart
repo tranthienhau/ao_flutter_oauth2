299e4b84a3672d55bdb251469c039ece91405ea6
tranthienhau <tranthienhau@gmail.com>
2022-06-06 18:27:01 +0700

    - add function login with apple

--- ao_flutter_oauth2/lib/src/authencation/firebase_authen.dart
+++ ao_flutter_oauth2/lib/src/authencation/firebase_authen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:the_apple_sign_in/the_apple_sign_in.dart';
 
 class FirebaseAuthencation {
   Future<void> initFirebase() async {
@@ -79,8 +80,7 @@ class FirebaseAuthencation {
     User? user;
     try {
       await FacebookAuth.instance.logOut();
-      final facebookloginResult =
-          await FacebookAuth.instance.login(permissions: ['email']);
+      final facebookloginResult = await FacebookAuth.instance.login();
       final userData = await FacebookAuth.instance.getUserData();
 
       final facebookAuthCredential = FacebookAuthProvider.credential(
@@ -106,5 +106,48 @@ class FirebaseAuthencation {
   Future<void> logout() async {
     final auth = FirebaseAuth.instance;
     await auth.signOut();
+  }
+
+  Future<bool> checkAppleSignInAvailable() async {
+    return await TheAppleSignIn.isAvailable();
+  }
+
+  Future<User?> loginWithApple({List<Scope> scopes = const []}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    // 1. perform the sign-in request
+    final result = await TheAppleSignIn.performRequests(
+        [AppleIdRequest(requestedScopes: scopes)]);
+    // 2. check the result
+    switch (result.status) {
+      case AuthorizationStatus.authorized:
+        final appleIdCredential = result.credential!;
+        final oAuthProvider = OAuthProvider('apple.com');
+        final credential = oAuthProvider.credential(
+          idToken: String.fromCharCodes(appleIdCredential.identityToken!),
+          accessToken:
+              String.fromCharCodes(appleIdCredential.authorizationCode!),
+        );
+        final userCredential = await auth.signInWithCredential(credential);
+        user = userCredential.user!;
+        return user;
+      // if (scopes.contains(Scope.fullName)) {
+      //   final fullName = appleIdCredential.fullName;
+      //   if (fullName != null &&
+      //       fullName.givenName != null &&
+      //       fullName.familyName != null) {
+      //     final displayName = '${fullName.givenName} ${fullName.familyName}';
+      //     await firebaseUser.updateDisplayName(displayName);
+      //   }
+      // }
+      // return firebaseUser;
+      case AuthorizationStatus.error:
+        throw result.error.toString();
+      case AuthorizationStatus.cancelled:
+        throw 'Sign in aborted by user';
+      default:
+        throw 'Unkow Error';
+    }
   }
 }
